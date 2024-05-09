578c32291ca3d31d2d1978b3dbf4fa51e24ea3b9
Jongita <jurgitajonaityte@gmail.com>
2024-05-09 20:23:05 +0300

    Initial

---
+++ src/app/models/company.ts
@@ -0,0 +1,9 @@
+export class Company {
+    public name:string|null=null;
+    public code:number|null=null;
+    public vatNumber:number|string|null=null;
+    public address:string|null=null;
+    public email:string|null=null;
+    public phone:string|null=null;
+    public id:string|null=null;
+}
