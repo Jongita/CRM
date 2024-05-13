2f035b2dad89d21aa2796b11b9534dbb4ff8706d
Jongita <jurgitajonaityte@gmail.com>
2024-05-13 22:58:56 +0300

    reactive form

--- src/app/components/contacts/new-contact/new-contact.component.ts
+++ src/app/components/contacts/new-contact/new-contact.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-contact.component.html',
   styleUrl: './new-contact.component.css'
 })
@@ -13,14 +14,39 @@ export class NewContactComponent {
 
   constructor(){
     this.contactForm=new FormGroup({
-      'name':new FormControl("555555"),
-      'surname':new FormControl(null),
-      'position':new FormControl(null),
-      'company':new FormControl(null),
-      'phones':new FormControl(555555),
+      'name':new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+      'surname':new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(30)]),
+      'position':new FormControl(null, Validators.required),
+      'company':new FormControl(null, Validators.required),
+      'phones':new FormArray([])
+      // (null, [Validators.required, this.validatePhone]),
 
     })
   }
+  onSubmit(){
+    console.log(this.contactForm);
+  }
+
+  validatePhone(control: FormControl): ValidationErrors | null {
+    const phone: string = control.value;
+    const pattern = /^\+[0-9]{11}$/;
+
+    if (pattern.test(phone)){
+      return null;
+    } else {
+      return {error: 'Klaida'};
+    }
+  }
+
+  get phones() {
+    return (this.contactForm.get('phones') as FormArray).controls;
+  }
+
+  public addPhone(){
+    const number = new FormControl(null, [Validators.required, this.validatePhone]);
+    (this.contactForm.get('phones') as FormArray).push(number);
+  }
+
 }
 
 
